b41446bbd89e6f7560c800a23872a64d24e6b39e
Vedaangh Rungta <49959052+ved07@users.noreply.github.com>
2020-05-25 23:06:00 +0100

    ibm setup

--- sandbox - Copy.py
+++
@@ -1,9 +0,0 @@
-from mlp import multiLayeredPerceptron
-
-network = multiLayeredPerceptron()
-initialisedData = network.dataFrameInit('data.csv')
-commonality = network.dataFrameInit('commonality.csv')
-symp_arr = ['Coughs','fever','Loss of smell']
-diag = network.initPredData(initialisedData, symp_arr)
-pred = network.predLoop(predData=diag, dataframe=initialisedData, commonality=commonality)
-print(pred)
